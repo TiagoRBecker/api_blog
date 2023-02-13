a42682424b0ebd2b4fd49bac49edbd74e62c7cf9
Tiago <beckertiago09@gmail.com>
2023-02-12 22:11:24 -0300

    teste

--- src/server.ts
+++ src/server.ts
@@ -12,13 +12,10 @@ const url_tree = process.env.URL_3
 dotenv.config()
 
 const app = express();
-app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
-  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Methods','*');
-  next(); 
-})
-app.use(cors())
+app.use(cors({
+  credentials:false,
+  origin:[url_one as string,url_two as string,url_tree as string] 
+}))
 
 
 
@@ -26,12 +23,12 @@ app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true} ));
-app.use(fileupload())
+
 
 
 app.use(routes)
 
-
+app.use(fileupload())
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on ${process.env.PORT}`)
