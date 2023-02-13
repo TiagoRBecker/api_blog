b6477d4de4f42d2866d00d7a1bf76b6b61a79658
Tiago <beckertiago09@gmail.com>
2023-02-12 22:13:18 -0300

    teste

--- dist/server.js
+++ dist/server.js
@@ -13,17 +13,14 @@ const url_two = process.env.URL_2;
 const url_tree = process.env.URL_3;
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000");
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    next();
-});
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    credentials: false,
+    origin: [url_one, url_two, url_tree]
+}));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use(fileupload());
 app.use(routes_1.default);
+app.use(fileupload());
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on ${process.env.PORT}`);
 });
